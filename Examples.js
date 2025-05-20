f2466aa29516509a7822c45fe57dc8c13645e5f5
pjsmelodev <pjsm.lesi@gmail.com>
2025-05-20 17:48:19 +0100

    Updated examples file

--- Examples.js
+++ Examples.js
@@ -20,3 +20,19 @@ export default function ShoppingList() {
     <ul>{listItems}</ul>
   );
 }
+
+// You can respond to events by declaring event handler functions inside your components:
+function MyButton() {
+    function handleClick() {
+        alert('You clicked me!');
+    }
+
+    return (
+        <button onClick={handleClick}>
+            Click me
+        </button>
+    );
+    // Notice how onClick={handleClick} has no parentheses at the end!
+    // Do not call the event handler function: you only need to pass it down.
+    // React will call your event handler when the user clicks the button.
+}
