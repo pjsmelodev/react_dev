2cac8f2e5d4ca484e9319c0d3b14480cd8792ca6
pjsmelodev <pjsm.lesi@gmail.com>
2025-05-20 16:42:12 +0100

    Updated examples file

--- Examples.js
+++ Examples.js
@@ -5,3 +5,13 @@ function MyFunction() {
         <button>I'm a button</button>
     );
 }
+
+// Components can be nested inside other components.
+export default function MyApp() {
+    return (
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyApp />
+        </div>
+    );
+}
