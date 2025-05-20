c26e781702bc14f525b92c016cc96d5fa8bd1307
pjsmelodev <pjsm.lesi@gmail.com>
2025-05-20 16:40:34 +0100

    Updated examples file

--- Examples.js
+++ Examples.js
@@ -1,1 +1,7 @@
 // React apps are made out of components.
+// A component is a piece of the UI (user interface) that has its own logic and appearance.
+function MyFunction() {
+    return (
+        <button>I'm a button</button>
+    );
+}
