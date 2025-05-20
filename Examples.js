614ced61f2e81653f6d220c270d1ff3ad0f9e9f4
pjsmelodev <pjsm.lesi@gmail.com>
2025-05-20 17:31:00 +0100

    Updated examples file

--- Examples.js
+++ Examples.js
@@ -58,3 +58,17 @@ return (
         {content}
     </div>
 );
+
+// If you prefer more compact code, you can use the conditional ? operator.
+<div>
+    {isLoggedIn ? (
+        <AdminPanel />
+    ) : (
+        <LoginForm />
+    )}
+</div>
+
+// When you don’t need the else branch, you can also use a shorter logical && syntax:
+{/* <div>
+    {isLoggedIn && <AdminPanel />}
+</div> */}
